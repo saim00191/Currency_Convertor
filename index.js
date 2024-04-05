83e9e4a7622f3b9d5590083d5ae44b1de9562350
saim00191 <saimamjad198@gmail.com>
2024-04-06 02:35:01 +0500

    EDIT

--- index.js
+++ index.js
@@ -1,38 +1,46 @@
 #! /usr/bin/env node
 import inquirer from "inquirer";
-let Currency = {
-    USD: 1,
-    EURO: 0.92,
-    PKR: 277.70,
-    BTC: 0.000015,
-    ETH: 0.00030,
-    INR: 83.29,
-    EGP: 47.33,
-    BDT: 109.50,
-    AED: 3.67
-};
-const answer = await inquirer.prompt([
-    {
-        name: "From",
-        type: "list",
-        message: "Select Your Currency : ",
-        choices: ["USD", "EURO", "PKR", "BTC", "ETH", "INR", "EGP", "BDT", "AED"]
-    },
-    {
-        name: "To",
-        type: "list",
-        message: "Select Your Convertion Currency : ",
-        choices: ["USD", "EURO", "PKR", "BTC", "ETH", "INR", "EGP", "BDT", "AED"]
-    },
-    {
-        name: "amount",
-        type: "number",
-        message: "Enter your Convertion Amount : "
-    }
-]);
-let FromAmount = Currency[answer.From];
-let ToAmount = Currency[answer.To];
-let Amount = answer.amount;
-let BaseAmount = Amount / FromAmount;
-let ConvertedAmount = BaseAmount * ToAmount;
-console.log(ConvertedAmount.toFixed(2));
+let Condition = true;
+while (Condition) {
+    let Currency = {
+        USD: 1,
+        EURO: 0.92,
+        PKR: 277.70,
+        INR: 83.29,
+        EGP: 47.33,
+        BDT: 109.50,
+        AED: 3.67
+    };
+    const answer = await inquirer.prompt([
+        {
+            name: "From",
+            type: "list",
+            message: "Select Your Currency : ",
+            choices: ["USD", "EURO", "PKR", "INR", "EGP", "BDT", "AED"]
+        },
+        {
+            name: "To",
+            type: "list",
+            message: "Select Your Convertion Currency : ",
+            choices: ["USD", "EURO", "PKR", "INR", "EGP", "BDT", "AED"]
+        },
+        {
+            name: "amount",
+            type: "number",
+            message: "Enter your Convertion Amount : "
+        }
+    ]);
+    let FromAmount = Currency[answer.From];
+    let ToAmount = Currency[answer.To];
+    let Amount = answer.amount;
+    let BaseAmount = Amount / FromAmount;
+    let ConvertedAmount = BaseAmount * ToAmount;
+    console.log(ConvertedAmount.toFixed(2));
+    const Confirm = await inquirer.prompt({
+        type: "confirm",
+        name: "again",
+        message: "Do you Want to Convert More? ",
+        default: false
+    });
+    Condition = Confirm.again;
+}
